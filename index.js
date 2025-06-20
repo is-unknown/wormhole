4804d852864fa1c76ecdcc44415d7d2991bcb054
tj <thefoxjones@gmail.com>
2025-06-20 11:31:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,7 @@
 
 if (require('is-odd')(1)) {
   var chalk = require('chalk')
-  console.log(chalk.red("Wormhole."))
+  console.log(chalk.red("If you see a wormhole, report it to the wormholefighters at https://wormholes.66ghz.com"))
+} else {
+  console.error('1 is not odd')
 }
